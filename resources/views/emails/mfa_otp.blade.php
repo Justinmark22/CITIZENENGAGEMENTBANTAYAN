be4d0a7c3fa85d74d247b4245a6d58a2f3394bd4
Justinmark22 <justinkaquilala07@gmail.com>
2025-10-03 16:18:59 +0800

    hasd

---
+++ resources/views/emails/mfa_otp.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>MFA OTP</title>
+</head>
+<body>
+    <p>Hello,</p>
+    <p>Your Bantayan 911 verification code is: <strong>{{ $otp }}</strong></p>
+    <p>This code will expire in 5 minutes.</p>
+</body>
+</html>
