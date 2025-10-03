71411fd330c2df28c52e2cddd29fef49f0b884c3
Justinmark22 <justinkaquilala07@gmail.com>
2025-10-03 08:08:24 +0800

    pass

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ForgotPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Forgot Password Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset emails and
+    | includes a trait which assists in sending these notifications.
+    |
+    */
+
+    use SendsPasswordResetEmails;
+
+    /**
+     * Display the form to request a password reset link.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email'); // points to your email.blade.php
+    }
+}
