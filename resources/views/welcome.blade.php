c79b5f0b5036ba195c9df2f76d77f633c883c856
Justinmark22 <justinkaquilala07@gmail.com>
2025-10-22 10:51:17 +0800

    sd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Bantayan 911</title>
   
-  <!-- Tailwind & Fonts -->
-  <script src="https://cdn.tailwindcss.com"></script>
+ 
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
   <!-- Alpine.js -->
