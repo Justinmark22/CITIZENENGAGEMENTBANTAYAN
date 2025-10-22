346d5e4289de102e50ec9e4c7dbae785fb91fb58
Justinmark22 <justinkaquilala07@gmail.com>
2025-10-22 10:19:40 +0800

    sd

--- resources/js/main.jsx
+++
@@ -1,8 +0,0 @@
-import './bootstrap';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './app';  // Import your App component
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
