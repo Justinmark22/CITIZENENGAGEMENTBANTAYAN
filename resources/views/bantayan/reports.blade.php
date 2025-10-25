93543ecfcaf89ee01fbdd05f8816e1c780f6558a
Justinmark22 <justinkaquilala07@gmail.com>
2025-10-25 12:40:16 +0800

    as

--- resources/views/bantayan/reports.blade.php
+++ resources/views/bantayan/reports.blade.php
@@ -247,13 +247,6 @@
           {{ $report->status }}
         </span>
 
-              </li>
-            @endif
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
 @empty
   <div class="alert alert-info">No reports found.</div>
 @endforelse
