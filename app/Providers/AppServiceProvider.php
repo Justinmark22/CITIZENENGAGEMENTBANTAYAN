bd7432bcd8afb1cf14335063c1c2c364219f6ecb
Justinmark22 <justinkaquilala07@gmail.com>
2025-10-27 21:31:06 +0800

    aS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Ensure important storage sub-directories exist (prevents file_put_contents errors on some hosts)
+        $paths = [
+            storage_path('framework/sessions'),
+            storage_path('framework/cache'),
+            storage_path('framework/views'),
+        ];
+
+        foreach ($paths as $p) {
+            if (!is_dir($p)) {
+                @mkdir($p, 0755, true);
+                // best-effort permission set
+                @chmod($p, 0755);
+                Log::info("Created storage directory: {$p}");
+            }
+        }
     }
 }
