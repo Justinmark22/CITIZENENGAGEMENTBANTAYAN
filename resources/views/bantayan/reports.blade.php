cb64f12112d4c1dea193f142c1d5f6a77e0b3f6c
Justinmark22 <justinkaquilala07@gmail.com>
2025-10-29 14:45:09 +0800

    As

--- resources/views/bantayan/reports.blade.php
+++ resources/views/bantayan/reports.blade.php
@@ -5,10 +5,10 @@
   <title>bantayan- Reports</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script src="https://unpkg.com/lucide@latest"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <meta name="csrf-token" content="{{ csrf_token() }}">
-
 
   <style>
     body {
